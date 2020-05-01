2686ac86ef866d69235a5433fe089ac10046f029
Moses igbuku <captainoak@gmail.com>
2020-05-01 23:07:58 +0100

    getting transfromed code

--- src/components/blocks/blocks.js
+++ src/components/blocks/blocks.js
@@ -1,20 +1,32 @@
-import React from 'react'
-import Editor from 'blocks-ui'
-import { HeaderBasic, TaglineBold, } from '@blocks/react'
-import CustomBlock from './custom'
-import CompactForm from './form'
-import Title from './title'
-import Button from './button'
-import Skip from './skip';
-import button from './title'
+import React from "react";
+import Editor from "blocks-ui";
+import { HeaderBasic, TaglineBold } from "@blocks/react";
+import CustomBlock from "./custom";
+import CompactForm from "./form";
+import Title from "./title";
+import Button from "./button";
+import Skip from "./skip";
+import button from "./title";
 const JSX = `
 import React from 'react'
 export default () => (
   <Blocks.Root>
   </Blocks.Root>
 )
-`
+`;
 const myJSX = `
 
-`
-export default () => <Editor src={JSX} blocks={{ CompactForm, Title, Button, CustomBlock, HeaderBasic, Skip }} />
+`;
+export default () => {
+  const onChange = (newCode) => {
+    console.log(newCode);
+  };
+
+  return (
+    <Editor
+      src={JSX}
+      blocks={{ CompactForm, Title, Button, CustomBlock, HeaderBasic, Skip }}
+      onChange={onChange} // get  code
+    />
+  );
+};
